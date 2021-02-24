21ed8768fe7842429e0f4ce6bc2ea7f7065cb579
Maksym Popovych <78795214+maxpopovych@users.noreply.github.com>
2021-02-24 14:33:00 +0200

    fix back routing from answers

--- QuizzAppFront/quizz-app/src/app/answer/index/index.component.ts
+++ QuizzAppFront/quizz-app/src/app/answer/index/index.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Answer } from '../answer';
 import { AnswerService } from '../answer.service';
 import { Location } from '@angular/common'
+import { QuestionService } from 'src/app/question/question.service';
 
 
 
@@ -16,18 +17,27 @@ export class IndexComponentA implements OnInit {
 
   answers!: Answer[];
   id!: number;
+  quest_id!:number;
+  test_id!:number;
 
   constructor(
     public answerService: AnswerService,
+    public questionService: QuestionService,
     private route: ActivatedRoute,
-    private router: Router,
-    private location: Location
+    private router: Router
     ) { }
   
   ngOnInit(): void {
     this.id = this.route.snapshot.params['answerId'];
     this.answerService.getAll(this.id).subscribe((data: Answer[])=>{
       this.answers = data;
+      if(!(data[0] == null || data[0] == undefined)){
+        this.quest_id = data[0].questionId;
+      }
+      else{
+        this.quest_id = 0;
+      }
+      this.questionService.find(this.quest_id).subscribe(x=>{this.test_id = x.testId;})
       console.log(this.answers);
     })  
   }
@@ -43,10 +53,6 @@ export class IndexComponentA implements OnInit {
     this.answerService.setTrue(id).subscribe(res => {
          console.log('Answer set true successfully!');
     })
-  }
-
-  back(): void {
-    this.location.back()
   }
   
 }
