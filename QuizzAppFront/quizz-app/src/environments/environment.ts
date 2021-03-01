5fcc450adb296012e31b3b0c38d443e7a73c122b
Maksym Popovych <78795214+maxpopovych@users.noreply.github.com>
2021-03-01 15:22:07 +0200

    Update environment.ts

--- QuizzAppFront/quizz-app/src/environments/environment.ts
+++ QuizzAppFront/quizz-app/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlAddress: 'https://quizzmain-env.eba-5ehhxmt7.us-east-2.elasticbeanstalk.com'
+  urlAddress: 'https://quizzapp20210301133452.azurewebsites.net'
 };
 
 /*
